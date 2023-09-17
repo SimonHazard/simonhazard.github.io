245cbeb57e8ab64f3a30bbbf7d848e2878f950c5
SimonHazard <simon.hazard@outlook.fr>
2023-09-17 22:39:14 +0200

    playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:5173',
+    baseURL: 'http://127.0.0.1:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry'
@@ -46,13 +46,13 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] }
     }
-  ]
+  ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run dev',
-  //   url: 'http://127.0.0.1:5173',
-  //   timeout: 60000,
-  //   reuseExistingServer: !process.env.CI
-  // }
+  webServer: {
+    command: 'pnpm run dev',
+    url: 'http://127.0.0.1:5173',
+    timeout: 60000,
+    reuseExistingServer: !process.env.CI
+  }
 });
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://simonhazard.com/');
+  await page.goto('/');
   await expect(page).toHaveTitle(/Simon HAZARD/);
 });
 
 test('article link', async ({ page }) => {
-  await page.goto('https://simonhazard.com/');
+  await page.goto('/');
   await page.getByRole('link', { name: 'Articles' }).click();
   await page.waitForURL(/articles/);
 });
