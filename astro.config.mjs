7f5a02bf571810a5cc13783a9a90f0308c63cb87
Simon HAZARD <simon.hazard@outlook.fr>
2026-03-14 17:16:34 +0100

    refactor: configure Cloudflare adapter to use passthrough image service.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,13 @@ import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://simonhazard.com",
-  adapter: cloudflare(),
-  image: {
-    service: passthroughImageService(),
-  },
+  adapter: cloudflare({
+    imageService: "passthrough",
+  }),
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
